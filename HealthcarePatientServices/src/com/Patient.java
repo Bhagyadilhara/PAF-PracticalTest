944e2196977a0e81c1930b429d2ca27310a605bb
Bhagyadilhara <37486204+Bhagyadilhara@users.noreply.github.com>
2020-05-04 10:32:29 +0530

    Create DB connection

---
+++ HealthcarePatientServices/src/com/Patient.java
@@ -0,0 +1,30 @@
+package com;
+
+import java.sql.*;
+
+public class Patient {
+
+	
+	//DB connection----------------------------
+	private Connection connect(){
+		
+		Connection con = null;
+				try
+					{
+					Class.forName("com.mysql.jdbc.Driver");
+					//Provide the correct details: DBServer/DBName, username, password
+					con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/healthcaredb","root","");
+					}
+				catch (Exception e){
+					e.printStackTrace();
+				}
+		return con;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
