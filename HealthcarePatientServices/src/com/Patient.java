2007c9b5954a6561986c3fc47d155012b9af6e69
Bhagyadilhara <37486204+Bhagyadilhara@users.noreply.github.com>
2020-05-04 11:54:22 +0530

    Updated insert patients function

--- HealthcarePatientServices/src/com/Patient.java
+++ HealthcarePatientServices/src/com/Patient.java
@@ -91,6 +91,48 @@ public class Patient {
 	}
 	
 	
+	//insert-------------------------------------------------------------------------------------------------------------------------
+	public String insertPatient(String username,String address,String nic,String dob,String gender,String mobile_number,String email,String password)
+    {
+			String output = "";
+			try
+			{
+				Connection con = connect();
+				if (con == null)
+				{    
+					return "Error while connecting to the database for inserting new patient.";
+				}
+				// create a prepared statement
+				String query = "insert into patient"
+						+"(`userID`,`userName`,`userAddress`,`nicNo`,`dateOfBirth`,`gender`,`mobileNumber`,`email`,`password`)"
+						 + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+						PreparedStatement preparedStmt = con.prepareStatement(query); 
+				// binding values
+				preparedStmt.setInt(1, 0);
+				preparedStmt.setString(2, username);
+				preparedStmt.setString(3, address);
+				preparedStmt.setString(4, nic);
+				preparedStmt.setString(5, dob);
+				preparedStmt.setString(6, gender); 
+				preparedStmt.setString(7, mobile_number); 
+				preparedStmt.setString(8, email); 
+				preparedStmt.setString(9, password); 
+	
+				// execute the statement
+				preparedStmt.execute();
+				con.close();
+				String newPatients = readPatients();
+				output = "{\"status\":\"success\", \"data\": \"" +newPatients + "\"}";
+			}
+				catch (Exception e)
+			{
+					output = "{\"status\":\"error\", \"data\": \"Error while inserting new patient.\"}";
+					System.err.println(e.getMessage());
+			}
+			return output;
+    		}
+	
+	
 	
 	
 	
