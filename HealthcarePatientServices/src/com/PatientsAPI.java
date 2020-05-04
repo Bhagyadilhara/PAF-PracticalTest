4c7a40578093993c2c705118bf784097630e2d44
Bhagyadilhara <37486204+Bhagyadilhara@users.noreply.github.com>
2020-05-04 22:20:29 +0530

    Convert request parameters to a Map

--- HealthcarePatientServices/src/com/PatientsAPI.java
+++ HealthcarePatientServices/src/com/PatientsAPI.java
@@ -1,41 +1,62 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class PatientsAPI
- */
+
 @WebServlet("/PatientsAPI")
 public class PatientsAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	Patient patientObj = new Patient();
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public PatientsAPI() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+ // Convert request parameters to a Map
+ 		private static Map getParasMap(HttpServletRequest request)
+ 		{
+ 			Map<String, String> map = new HashMap<String, String>();
+ 			
+ 			try
+ 			{
+ 				Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+ 				String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+ 				scanner.close();
+ 				
+ 				String[] params = queryString.split("&");
+ 				
+ 				for (String param : params)
+ 				{
+ 					String[] p = param.split("=");
+ 					map.put(p[0], p[1]);
+ 				}
+ 			}
+ 			catch (Exception e)
+ 			{
+ 			}
+ 			
+ 			return map;
+ 		}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		//NOT USED
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String output = patientObj.insertPatient(request.getParameter("username"),
@@ -52,16 +73,15 @@ public class PatientsAPI extends HttpServlet {
 				//(username, address, nic, dob, gender, mobile_number, email, password)
 	}
 
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
+	
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
+	
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
