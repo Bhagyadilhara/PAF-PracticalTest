93e4c4241e03b3ec0f674cf60afdbaa084cb5ed0
Bhagyadilhara <37486204+Bhagyadilhara@users.noreply.github.com>
2020-05-05 09:23:51 +0530

    Added save validation part

--- HealthcarePatientServices/WebContent/Components/patients.js
+++ HealthcarePatientServices/WebContent/Components/patients.js
@@ -4,3 +4,42 @@ $(document).ready(function()
 	$("#alertError").hide();
 });
 
+//SAVE ============================================
+$(document).on("click", "#btnSave", function(event)
+{
+	// Clear alerts---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+	
+	// Form validation-------------------
+	var status = validatePatientForm();
+	
+	if (status != true)
+	{
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+	
+	// If valid------------------------
+	var method = ($("#hidPatientIDSave").val() == "") ? "POST" : "PUT";
+	
+	$.ajax(
+	{
+		url : "PatientsAPI",
+		type : method,
+		data : $("#formPatient").serialize(),
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onPatientSaveComplete(response.responseText, status);
+		}
+	});
+});
+
+
+
+
+
